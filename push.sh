9c2a645f12b5d3958cb8acc742c646f2a36492d1
guokongming <guokongming@qq.com>
2022-01-22 00:55:10 +0800

    更新内容 + Action 自动部署

--- push.sh
+++ push.sh
@@ -15,6 +15,6 @@ git add -A
 
 git commit -m "${comment}"    # $1 是启动该脚本传来的参数，如 sh push.sh hello，其中 hello 就会插入到 $1 处，如果想两个参数，则加 $2
 
-git push $GITHUB_REPO   
-
 git push -u origin dev #建立远程分支 dev 和 关联本地dev和远程dev两步
+
+git push $GITHUB_REPO   
