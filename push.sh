d95e7c0c333717c083cb381074a4363f77344789
guokongming <guokongming@qq.com>
2022-01-22 00:53:22 +0800

    更新内容 + Action 自动部署

--- push.sh
+++ push.sh
@@ -3,23 +3,17 @@
 # 本脚本为自动部署的入口脚本(提交项目到 main 仓库的脚本)，只需执行该脚本，即可实现 GitHub 的自动部署，
 
 
+GITHUB_REPO=git@github.com:guokongming/guokongming.github.io.git
 
 comment=$1
 
 if [ ! $comment ]; then
-comment="更新源代码"
+comment="更新内容 + Action 自动部署"
 fi
-
-git init  
 
 git add -A
 
 git commit -m "${comment}"    # $1 是启动该脚本传来的参数，如 sh push.sh hello，其中 hello 就会插入到 $1 处，如果想两个参数，则加 $2
 
-git push -u origin master    //把本地仓库的文件上传到远程仓库。
-
-
-
-
-
+git push $GITHUB_REPO   
 
