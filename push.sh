278515d0499623253321b3dbef04a15d66d53c13
guokongming <guokongming@qq.com>
2022-01-22 00:32:46 +0800

    更新源代码

--- push.sh
+++ push.sh
@@ -16,8 +16,12 @@ git init
 git add -A
 
 git commit -m "${comment}"    # $1 是启动该脚本传来的参数，如 sh push.sh hello，其中 hello 就会插入到 $1 处，如果想两个参数，则加 $2
- 
-git remote set-url [--push] $GITHUB_REPO  
+
+git push -u origin $GITHUB_REPO  main 
+
+
+
+
 
 
 
